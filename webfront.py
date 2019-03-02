3b1f19987a24b229fd1c726e8896988f0a910ba7
Brian Copeland <brian.copeland@target.com>
2019-03-02 12:03:34 -0600

    Jerk updated with summary

--- webfront.py
+++ webfront.py
@@ -15,15 +15,56 @@ def dict_factory(cursor, row):
         d[col[0]] = row[idx]
     return d
 
+def average_temp(temp_score):
+    if temp_score <= 1.3:
+        return "mostly not a jerk"
+    elif temp_score > 1.3 and temp_score <= 1.7:
+        return "between not a jerk and a tiny bit of a jerk"
+    elif temp_score > 1.7 and temp_score <= 2.3:
+        return "mostly just a tiny bit of a jerk"
+    elif temp_score > 2.3 and temp_score <= 2.7:
+        return "between a tiny bit and a fair amount of a jerk"
+    elif temp_score > 2.7 and temp_score <= 3.3:
+        return "mostly a fair amount of jerk"
+    elif temp_score > 3.3 and temp_score <= 3.7:
+        return "between a fair amount and pretty jerky"
+    elif temp_score > 3.7 and temp_score <= 4.3:
+        return "pretty jerky"
+    elif temp_score > 4.3 and temp_score <= 4.7:
+        return "between pretty jerky and a total jerk"
+    elif temp_score > 4.7:
+        return "mostly a total jerk"
+    else:
+        return "broken"
+
 @app.route('/')
 def home():
     with sqlite3.connect('jerk.db') as conn:
         cget = conn.cursor()
         cget.row_factory = dict_factory
-        cget.execute("""SELECT jerktemp, datetime(sqltime, 'localtime') as updatedt FROM jerk ORDER BY sqltime DESC LIMIT 50""")
-        x = cget.fetchone()
-        y = cget.fetchall()
-    return render_template('home.html', temp=x['jerktemp'], update=x['updatedt'], history=y)
+        cget.execute("""SELECT jerktemp, datetime(sqltime, 'localtime', '-1 hour') as updatedt FROM jerk ORDER BY sqltime DESC LIMIT 50""")
+        x = cget.fetchall()
+        current_update_dt = datetime.strptime(x[0]['updatedt'], '%Y-%m-%d %H:%M:%S').strftime('%A %b %d %I:%M %p')
+        cget.execute("""SELECT SUM(CASE WHEN jerktemp = 'too-cold' then 1 WHEN jerktemp = 'cold' then 2 WHEN jerktemp = 'perfect' then 3
+        WHEN jerktemp = 'lukewarm' then 4 WHEN jerktemp = 'hot' then 5 ELSE 0 END) as score, COUNT(*) as theCount FROM jerk;""")
+        agg = cget.fetchone()
+        total_updates = agg['theCount']
+        total_temp_string =  average_temp(agg['score'] / agg['theCount'])
+        cget.execute("""SELECT SUM(CASE WHEN jerktemp = 'too-cold' then 1 WHEN jerktemp = 'cold' then 2 WHEN jerktemp = 'perfect' then 3
+        WHEN jerktemp = 'lukewarm' then 4 WHEN jerktemp = 'hot' then 5 ELSE 0 END) as score, COUNT(*) as theCount FROM jerk
+        WHERE sqltime > datetime(CURRENT_TIMESTAMP, '-1 day');""")
+        today_agg = cget.fetchone()
+        today_updates = today_agg['theCount']
+        today_temp_string =  average_temp(today_agg['score'] / today_agg['theCount'])
+    return render_template('home.html',
+        temp=x[0]['jerktemp'],
+        update=current_update_dt,
+        history=x,
+        total_updates=total_updates,
+        total_temp_string=total_temp_string,
+        today_updates=today_updates,
+        today_temp_string=today_temp_string
+        )
 
 @app.route('/test')
 def test_site():
